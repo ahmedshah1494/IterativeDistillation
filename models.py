38b290454cbd3e245e648401ef01d34bb54737c4
Muhammad Ahmed Shah <mshah1@compute-2-5.local>
2019-10-14 15:30:04 -0400

    fixed the loading of pretrained models; added support for multi-layered classifiers

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
+import numpy as np
+import torchvision
 import utils
+import types
 
 class ModelWrapper(object):
     def __init__(self):
@@ -150,4 +153,203 @@ class PapernotCIFAR10_2(PapernotCIFAR10):
                 nn.utils.weight_norm(nn.Linear(256, n_classes, bias=False), name='weight')
         )
 
+class VGG16(nn.Module, ModelWrapper):
+    def __init__(self, num_classes):
+        super(VGG16, self).__init__()
     
+        self.layers = nn.Sequential(
+            nn.Conv2d(3,64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(64,64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64,128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(128,128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128,256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(256,256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256,512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(512,512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(512,512, kernel_size=1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(512,512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512,512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(512,512, kernel_size=1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.AdaptiveAvgPool2d((1,1)),
+            Flatten(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+    def forward(self, x):
+        return self.layers(x)
+
+class VGG16CIFAR(nn.Module, ModelWrapper):
+    def __init__(self, num_classes):
+        super(VGG16CIFAR, self).__init__()
+    
+        self.layers = nn.Sequential(
+            nn.Conv2d(3,64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(64,64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.5),
+            nn.Conv2d(64,128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(128,128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.5),
+            nn.Conv2d(128,256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(256,256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.5),
+            nn.Conv2d(256,512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(512,512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(512,512, kernel_size=1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.5),
+            nn.Conv2d(512,512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512,512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.Conv2d(512,512, kernel_size=1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.AdaptiveAvgPool2d((1,1)),
+            Flatten(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(256, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, num_classes),
+        )
+    def forward(self, x):
+        return self.layers(x)
+
+def modified_fwd(self, x):
+    bs = x.shape[0]
+    if len(x.shape) == 5:        
+        x = utils.reshape_multi_crop_tensor(x)
+    out = self.old_forward(x)
+    out = out.view(bs, -1, out.shape[1])
+    out = torch.mean(out, dim=1)
+    return out
+
+def get_classifier(indim, num_classes, depth):
+    width = 2**(int(np.log2(num_classes)) + 2)
+    layers = []
+    for i in range(depth):
+        if i > 0:
+            indim = width
+        if i == depth-1:
+            outdim = num_classes
+        else:
+            outdim = width
+        layers.append(nn.Linear(indim, outdim))
+        if i < depth-1:
+            layers.append(nn.ReLU(True))
+            layers.append(nn.Dropout())
+    classifier = nn.Sequential(*layers)
+    return classifier
+
+def resnet18TinyImageNet(num_classes, pretrained=False, feature_extraction=False, **kwargs):
+    m = torchvision.models.resnet18(pretrained=pretrained)
+    m.avgpool = nn.AdaptiveAvgPool2d(1)
+    num_ftrs = m.fc.in_features
+    m.fc = nn.Linear(num_ftrs, 200)
+    m.conv1 = nn.Conv2d(3,64, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+    m.maxpool = nn.Sequential()    
+    for param in m.fc.parameters():
+        param.requires_grad = True
+    return m
+
+def resnet18(num_classes, pretrained=False, feature_extraction=False, classifier_depth=1):
+    m = torchvision.models.resnet18(pretrained=pretrained)
+    if pretrained and feature_extraction:
+        for param in m.parameters():
+            param.requires_grad = False
+    m.fc, _ = utils.change_layer_output(m.fc, num_classes)    
+    for param in m.fc.parameters():
+        param.requires_grad = True      
+    return m
+
+def vgg16(num_classes, pretrained=False, feature_extraction=False, classifier_depth=1):
+    m = torchvision.models.vgg16_bn(pretrained=pretrained)
+    if pretrained and feature_extraction:
+        for param in m.parameters():
+            param.requires_grad = False
+    # m.classifier[-1], _ = utils.change_layer_output(m.classifier[-1], num_classes)
+    num_ftrs = m.classifier[-1].in_features
+    m.classifier[-1] = get_classifier(num_ftrs, num_classes, classifier_depth)
+    for param in m.classifier[-1].parameters():
+        param.requires_grad = True
+    return m
+
+def vgg16CIFAR(num_classes, pretrained=False, feature_extraction=False, **kwargs):
+    return VGG16CIFAR(num_classes)
+
+def alexnet(num_classes, pretrained=False, feature_extraction=False, classifier_depth=1):
+    m = torchvision.models.alexnet(pretrained=pretrained)
+    if pretrained and feature_extraction:
+        for param in m.parameters():
+            param.requires_grad = False
+    m.classifier[-1], _ = utils.change_layer_output(m.classifier[-1], num_classes)
+    for param in m.classifier[-1].parameters():
+        param.requires_grad = True
+    return m
+
+def alexnetCIFAR(num_classes, pretrained=False, feature_extraction=False, **kwargs):
+    return AlexNetCIFAR(num_classes)
+
+def papernot(num_classes, pretrained=False, feature_extraction=False, **kwargs):
+    m = PapernotCIFAR10(num_classes)
+    return m
