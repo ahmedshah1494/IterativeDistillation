371fcffa7e234891af63327a9ece2f17e3796e72
Muhammad Ahmed Shah <mshah1@compute-2-5.local>
2019-10-15 08:26:34 -0400

    refactored some code, spliting training set into train and val

--- training.py
+++ training.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 import torchvision
+from torchvision import transforms
 import argparse
 import logging
 import sys
@@ -10,11 +11,18 @@ import utils
 import numpy as np
 from tqdm import tqdm
 import os
+from PIL import Image
+from distillation import simple_distillation
+from multiprocessing import cpu_count
 
 model_dict = {
-    'AlexNetCIFAR' : models.AlexNetCIFAR,
-    'AlexNet' : models.AlexNet,
-    'PapernotCIFAR10' : models.PapernotCIFAR10,
+    'resnet18TinyImageNet': models.resnet18TinyImageNet,
+    'resnet18': models.resnet18,
+    'vgg16': models.vgg16,
+    'vgg16CIFAR': models.vgg16CIFAR,
+    'AlexNetCIFAR' : models.alexnetCIFAR,
+    'AlexNet' : models.alexnet,
+    'PapernotCIFAR10' : models.papernot,
     'PapernotCIFAR10_2' : models.PapernotCIFAR10_2,
 }
 
@@ -31,105 +39,103 @@ def train_on_batch(model, batch, optimizer, criterion):
     optimizer.step()
     return float(loss*x.shape[0]), float(utils.compute_correct(out, y))
 
-def evaluate_on_batch(model, batch):
+def evaluate_on_batch(model, batch, metric=utils.compute_correct):
     model.eval()
     x,y = batch
     if use_cuda:
         x = x.cuda()
         y = y.cuda()        
     out = model(x)
-    return float(utils.compute_correct(out,y))
+    return metric(out,y)
 
-def loss(margin):
-    if margin == 0:
-        return nn.functional.cross_entropy
+def evaluate(model, loader, label_counts=None):
+    val_correct = 0
+    if label_counts is not None:
+        val_correct = np.zeros(len(label_counts))
+        metric = lambda out, true: utils.compute_correct_per_class(out, true, len(label_counts))
     else:
-        return lambda x,y: nn.functional.multi_margin_loss(x/torch.sum(x,1,keepdim=True), y, margin=margin)
-
-def train(model, train_dataset, val_dataset, args):
-    train_loader = DataLoader(train_dataset, args.batch_size)
-    val_loader = DataLoader(val_dataset, args.batch_size)
-
-    patience = 5
-    # criterion = nn.CrossEntropyLoss()
-    criterion = loss(args.C)
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.0001)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=patience, factor=0.5)    
+        metric = utils.compute_correct
+        
+    for batch in loader:
+        val_correct += evaluate_on_batch(model, batch, metric)
     
-    for i in range(args.nepochs):
-        epoch_loss = 0
-        epoch_correct = 0
+    return val_correct
 
-        t = tqdm(enumerate(train_loader))
-        t.set_description('epoch#%d' % i)
-        for j,batch in t:
-            train_loss, train_correct = train_on_batch(model, batch, optimizer, criterion)
-            epoch_loss += train_loss
-            epoch_correct += train_correct
-            t.set_postfix(loss=epoch_loss/((j+1)*args.batch_size), accuracy=epoch_correct/((j+1)*args.batch_size), 
-                            lr=optimizer.param_groups[0]['lr'])                        
-        epoch_loss /= len(train_dataset)
-        epoch_acc = epoch_correct/len(train_dataset)
-        
-        val_correct = 0
-        for batch in val_loader:
-            val_correct += evaluate_on_batch(model, batch)
-        val_acc = val_correct / len(val_dataset) 
-        print('val_accuracy =',val_acc)
-        
-        if i == 0 or scheduler.is_better(val_acc, scheduler.best):            
-            with open(args.outfile, 'wb') as f:
-                torch.save(model,f)
+def get_trainable_params(model,):
+    params_to_update = []
+    for name,param in model.named_parameters():
+        if param.requires_grad == True:
+            params_to_update.append(param)
+            
+    return params_to_update
 
-        scheduler.step(val_acc)
+def train(model, train_dataset, test_dataset, nclasses, args, val_dataset=None):    
+    if val_dataset is None:
+        new_train_size = int(0.8*len(train_dataset))
+        val_size = len(train_dataset) - new_train_size
+        train_dataset, val_dataset = random_split(train_dataset, [new_train_size, val_size])
 
-        logger.info('epoch#%d train_loss=%.3f train_acc=%.3f val_acc=%.3f lr=%.4f' % (i, epoch_loss, epoch_acc, val_acc, optimizer.param_groups[0]['lr']))
+    train_loader = DataLoader(train_dataset, args.batch_size, num_workers=(cpu_count())//2)
+    val_loader = DataLoader(val_dataset, args.batch_size, shuffle=False, num_workers=(cpu_count())//2) 
+    if not args.test_only:
+        criterion = utils.loss_wrapper(args.C)
+        optimizer = torch.optim.SGD(get_trainable_params(model), lr=args.lr, weight_decay=5e-4, momentum=0.9)    
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=args.patience, factor=0.5)
 
-def get_transform():
-    transform = torchvision.transforms.Compose([        
-        torchvision.transforms.RandomAffine(30), 
-        torchvision.transforms.RandomGrayscale(p=0.1),                
-        ])
-    transform = utils.curry(lambda x: torch.from_numpy(np.array(x)).float(), transform)
-    transform = utils.curry(lambda x: x/255, transform)
-    norm_mean = torch.tensor([0.485, 0.456, 0.406]).view(1,1,-1)
-    norm_std = torch.tensor([0.229, 0.224, 0.225]).view(1,1,-1)
-    transform = utils.curry(lambda x: (x - norm_mean) / norm_std, transform)
-    return transform
+        val_label_counts = utils.label_counts(val_loader, nclasses)
+
+        for i in range(args.nepochs):
+            epoch_loss = 0
+            epoch_correct = 0
+            
+            t = tqdm(enumerate(train_loader))
+            t.set_description('epoch#%d' % i)
+            for j,batch in t:
+                train_loss, train_correct = train_on_batch(model, batch, optimizer, criterion)
+                epoch_loss += train_loss
+                epoch_correct += train_correct
+                t.set_postfix(loss=epoch_loss/((j+1)*args.batch_size), accuracy=epoch_correct/((j+1)*args.batch_size), 
+                                lr=optimizer.param_groups[0]['lr'])
+            epoch_loss /= len(train_dataset)
+            epoch_acc = epoch_correct/len(train_dataset)
+            
+            val_correct = evaluate(model, val_loader, val_label_counts)
+            val_acc = np.mean(val_correct / val_label_counts)
+            print('val_accuracy:', val_acc)
+
+            if i == 0 or scheduler.is_better(val_acc, scheduler.best):            
+                with open(args.outfile, 'wb') as f:
+                    torch.save(model,f)
+
+            scheduler.step(val_acc)
+
+            logger.info('epoch#%d train_loss=%.3f train_acc=%.3f val_acc=%.3f lr=%.4f' % (i, epoch_loss, epoch_acc, val_acc, optimizer.param_groups[0]['lr']))
+    
+    test_loader = DataLoader(test_dataset, args.batch_size, shuffle=False, num_workers=(cpu_count())//2)
+
+    test_label_counts = utils.label_counts(test_loader, nclasses)
+    
+    test_correct = evaluate(model, test_loader, test_label_counts)
+    test_acc = np.mean(test_correct / test_label_counts)
+    print('test_accuracy:', test_acc)
+    logger.info('test_accuracy = %0.3f' % test_acc)
+
 
 def main(args):
-    transform = get_transform()
-    if args.dataset == 'CIFAR10':
-        transform = utils.curry(lambda x: x.transpose(2,1).transpose(0,1), transform)
-        train_dataset = torchvision.datasets.CIFAR10('/home/mshah1/workhorse3/', 
-                    transform=transform, download=True)
-        val_dataset = torchvision.datasets.CIFAR10('/home/mshah1/workhorse3/', train=False,
-                    transform=transform, download=True)        
-        nclasses = 10
-    if args.dataset == 'CIFAR100':
-        transform = utils.curry(lambda x: x.transpose(2,1).transpose(0,1), transform)
-        train_dataset = torchvision.datasets.CIFAR10('/home/mshah1/workhorse3/', 
-                    transform=transform, download=True)
-        val_dataset = torchvision.datasets.CIFAR10('/home/mshah1/workhorse3/', train=False,
-                    transform=transform, download=True)        
-        nclasses = 100
-    elif args.dataset == 'ImageNet':
-        train_dataset = torchvision.datasets.ImageNet('/home/mshah1/workhorse3/', train=True,
-                                                transform=transform, download=True)
-        test_dataset = torchvision.datasets.ImageNet('/home/mshah1/workhorse3/', train=False,
-                                                transform=transform, download=True)
-        nclasses = 1000
-    else:
-        raise NotImplementedError
-        
+    train_dataset, test_dataset, nclasses = utils.get_datasets(args)
+
     if args.model_path is not None:
         model = torch.load(args.model_path)
+        if not args.feature_extraction:
+            for param in model.parameters():
+                param.requires_grad = True
     else:
-        model = model_dict[args.model](nclasses)
+        model = model_dict[args.model](nclasses, args.pretrained, args.feature_extraction, args.classifier_depth)
 
     if use_cuda:        
         model = model.cuda()
-    train(model, train_dataset, val_dataset, args)
+    logger.info(model)
+    train(model, train_dataset, test_dataset, nclasses, args)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -137,12 +143,18 @@ if __name__ == '__main__':
     parser.add_argument('--model', type=str)
     parser.add_argument('--model_path', type=str)
     parser.add_argument('--nepochs', type=int, default=50)
+    parser.add_argument('--patience', type=int, default=5)
     parser.add_argument('--batch_size', type=int, default=256)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--C', type=float, default=0)
     parser.add_argument('--outfile', type=str, default='model.pt')
     parser.add_argument('--logfile', type=str, default='training.log')
     parser.add_argument('--cuda', action='store_true')
+    parser.add_argument('--pretrained', action='store_true')
+    parser.add_argument('--feature_extraction', action='store_true')
+    parser.add_argument('--expand_w_multiple_scales', action='store_true')
+    parser.add_argument('--test_only', action='store_true')
+    parser.add_argument('--classifier_depth', type=int, default=1)
     args = parser.parse_args()
 
     if not os.path.exists(os.path.dirname(args.logfile)):
@@ -160,5 +172,10 @@ if __name__ == '__main__':
     logger = logging.getLogger()
 
     use_cuda = args.cuda and torch.cuda.is_available()
+    if args.cuda and torch.cuda.is_available():
+        args.device = torch.device('cuda')
+    else:
+        args.device = torch.device('cpu')
+
     logger.info(args)
     main(args)
